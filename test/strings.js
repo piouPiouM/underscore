d9433cca305add81d76ccf7074ae5c17ce18c8bc
Mehdi Kabab <pioupioum@gmail.com>
2012-01-24 23:39:40 +0100

    Added one test for the _.capitalize() method.

--- test/strings.js
+++ test/strings.js
@@ -16,13 +16,13 @@ $(document).ready(function() {
     equals(_.upcase("abc hello 123"), "ABC HELLO 123");
   });
 
-  test("strings: isUpperCase", function () {
+  test("strings: isUpperCase", function() {
     ok(_.isUpperCase("HELLO"), '"HELLO" is in upper case.');
     ok(!_.isUpperCase("hello"), '"hello" is not in upper case.');
     ok(!_.isUpperCase("hELlo"), '"hELlo" is not in upper case.');
   });
 
-  test("strings: isLowerCase", function () {
+  test("strings: isLowerCase", function() {
     ok(_.isLowerCase("hello"), '"hello" is in lower case.');
     ok(!_.isLowerCase("HELLO"), '"HELLO" is not in lower case.');
     ok(!_.isLowerCase("hELlo"), '"hELlo" is not in upper case.');
@@ -32,9 +32,10 @@ $(document).ready(function() {
     equals(_.capitalize("hello"), 'Hello');
     equals(_.capitalize("HELLO"), 'Hello');
     equals(_.capitalize("123ABC"), '123abc');
+    equals(_.capitalize("abc Hello 123"), "Abc hello 123");
   });
 
-  test("strings: swapcase", function () {
+  test("strings: swapcase", function() {
     ok(_.isUpperCase(_.swapcase("hello")));
     ok(_.isLowerCase(_.swapcase("HELLO")));
     equals(_.swapcase("Hello"), "hELLO");
